6c585edf5cdd089328dabc41341a55f0f151dcb9
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-01-30 21:51:17 +0100

    start  with strings

---
+++ script/basic/strings.js
@@ -0,0 +1,16 @@
+
+//  First String and beginning
+
+let name = 'Bojan';
+
+let lastName = 'Mijatovic';
+
+let fullName = name + ' ' + lastName;
+
+let city = 'Novi Sad';
+let country = 'Serbia';
+let location = ' i come form ' + city + ' in ' + country;
+
+console.log(fullName);
+
+console.log('Hello my name is ' + fullName + location);
