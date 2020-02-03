9bdcb4b41d6fc443fefb1226157d6e867026b3f0
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-02-03 20:27:10 +0100

    exchange

---
+++ script/basic/exchangeOffice.js
@@ -0,0 +1,24 @@
+
+//  Cash form Customer
+let customerCash = 100;
+
+// Din to  Euros
+let exchangeDinToEuros = customerCash * 117.6;
+// Euros to Din
+let exchangeEurosToDin = 118.5 * customerCash;
+
+// Din to  Dollars
+let exchangeDinToDollars = customerCash * 103.8;
+//  Dollars to Din
+let exchangeDollarsToDin = 107.85 * customerCash;
+
+
+// Din to Canadian Dollar
+let exchangeCanadianDollars = customerCash * 76.1;
+//  Canadian Dollar to Din
+let exchangeCanadianDollarsToDin = 81.35 * customerCash;
+
+
+console.log('You have ' + exchangeEurosToDin + ' din now');
+
+console.log('You have ' + exchangeCanadianDollarsToDin + ' din now');
