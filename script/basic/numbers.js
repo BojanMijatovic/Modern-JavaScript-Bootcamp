0ea2651b0d56342a4e6c65f1be67b4fa0a8c8a8b
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-02-03 21:48:40 +0100

    Remainder

--- script/basic/numbers.js
+++ script/basic/numbers.js
@@ -4,8 +4,8 @@ let x = 4;
 
 let num = 5;
 
-let result = (x * num) + 20;
-// console.log('Result off numbers is ' + result);
+let sum = (x * num) + 20;
+// console.log('Result off numbers is ' + sum);
 
 //  Student Score
 
@@ -17,18 +17,52 @@ let resultScore = (studentScore / maxScore) * 100;
 
 
 //  More on numbers
-
 let num1 = 10;
 
 let num2 = num1 += 10;
 
 let num3 = num1 + num2;
 
-
 let num4 = num3 + 10;
-
 
 num4 -= 40;
 
-
 console.log(num4);
+
+
+const yearOffBirth = 1987;
+
+let year = 2020;
+
+let yearOffLife = year - yearOffBirth;
+console.log('You have now ' + yearOffLife + ' years');
+
+// Create calc
+
+let result = 0;
+result = result + 10;
+result += 20;
+
+
+//  Module 
+
+result %= 4;
+// console.log(result);
+
+let newResult = 100;
+newResult /= 4;
+
+// console.log(newResult);
+newResult %= 7;
+console.log(newResult);
+
+/*
+Remainder (%)
+
+The remainder operator returns the remainder left over when one operand is divided by a second operand.It always takes the sign of the dividend.
+  Syntax
+
+Operator: var1 % var2
+12 % 5 // 2
+
+*/
