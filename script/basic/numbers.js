e8be8cd7abe9f264e55231d9fb0ea79967c6bd14
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-02-03 20:13:15 +0100

    temp converter

--- script/basic/numbers.js
+++ script/basic/numbers.js
@@ -5,5 +5,13 @@ let x = 4;
 let num = 5;
 
 let result = (x * num) + 20;
-console.log('Result off numbers is ' + result);
+// console.log('Result off numbers is ' + result);
+
+//  Student Score
+
+let studentScore = 18;
+let maxScore = 20;
+let resultScore = (studentScore / maxScore) * 100;
+
+console.log('You have ' + resultScore + '%' + ' on test.');
 
---
+++ script/basic/tempConverter.js
@@ -0,0 +1,16 @@
+
+let fahrenheit = 60;
+
+//  converter to celsius
+
+let celsius = (fahrenheit - 32) / 1.8;
+
+console.log(celsius);
+
+
+//  converter to kelvin
+
+let kelvin = (fahrenheit + 459.67) * 5 / 9;
+console.log(kelvin);
+
+// T(K) = (60°F + 459.67)× 5/9 = 288.71 K
