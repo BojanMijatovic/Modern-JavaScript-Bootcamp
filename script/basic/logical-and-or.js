aa5634dc9d6295666c183093cf22deb3d5350ac5
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-02-04 21:02:53 +0100

    practice

---
+++ script/basic/logical-and-or.js
@@ -0,0 +1,56 @@
+
+
+//  Challenge area
+
+let isGestOneVegan = false;
+let isGestTwoVegan = false;
+
+// if (isGestOneVegan && isGestTwoVegan) {
+//   console.log('Are you both vegans? We can offer vegan dishes');
+// } else if (isGestOneVegan || isGestTwoVegan) {
+//   console.log('At least one vegan?');
+// } else {
+//   console.log('Offer up anything from menu');
+// }
+
+
+let temp = 65;
+
+// if (temp > 0 && temp < 25) {
+//   console.log('Weather is great');
+// } else if (temp <= 0) {
+//   console.log('Its cold outside');
+// } else {
+//   console.log('I don`t know how is weather  outside');
+// }
+
+let isAccountLocked = false;
+let amountStatus = 10;
+
+if (isAccountLocked === true) {
+  console.log('Sorry your account is locked');
+} else if (isAccountLocked === false && amountStatus >= 0) {
+  console.log('Welcome to your Account you have on account ' + amountStatus + '$');
+} else {
+  console.log('Welcome to app');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
