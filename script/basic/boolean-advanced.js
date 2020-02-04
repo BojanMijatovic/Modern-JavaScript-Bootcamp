370467c81af96344c56332bb6f19ba69a1124974
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-02-04 20:36:13 +0100

    if else 1

---
+++ script/basic/boolean-advanced.js
@@ -0,0 +1,15 @@
+
+let isAccountLocked = true;
+
+let temp = 21;
+
+
+if (temp <= 21) {
+  console.log('Its cold outside');
+
+} else if (temp >= 30) {
+  console.log('Its hot outside');
+
+} else {
+  console.log('Its great out side');
+}
