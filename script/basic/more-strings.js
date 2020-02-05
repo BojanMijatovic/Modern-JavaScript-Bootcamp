30d43099b995e78fe0017ee96bed6b043b304d4f
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-02-05 22:06:27 +0100

    add

--- script/basic/more-strings.js
+++ script/basic/more-strings.js
@@ -21,18 +21,24 @@ let searchName = name.toLowerCase().indexOf('b');
 if (searchName >= 0) {
   let newName = name.replace('Bojan', 'Js');
   let newLast = lastName.slice(9);
-  // console.log('New name is ' + newName + ' and last name is ' + newLast);
+  console.log('New name is ' + newName + ' and last name is ' + newLast);
 }
+let points = 0;
 
 let a = 'Soccer is best sport';
 
 let b = 'Basket is best sport';
 
-let test = a.trim().toLowerCase().indexOf('s');
+let test = a.trim().toLowerCase().indexOf('1s');
 
 if (test >= 0) {
-  let c = b.replace('Basket', 'Airsoft');
+  let c = b.replace('Basket', 'Airsoft').toUpperCase();
   console.log(c);
+  let newScore = points += 10;
+  console.log('Your score is now ' + newScore);
 } else {
-  console.log(a);
+  console.log(a.toUpperCase());
+  let newScore = points -= 10;
+  console.log('Your score is now  \n ' + newScore + ' points');
 }
+
