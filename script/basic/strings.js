97f0d396ba9e7dd840c07dc094f236069ad1ebdd
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-02-05 21:17:04 +0100

    replace

--- script/basic/strings.js
+++ script/basic/strings.js
@@ -40,19 +40,32 @@ let msg = 'Here is first chat';
 let freeSpace = 'Colors         ';
 console.log(freeSpace.length);
 
-let test = freeSpace.trim();
+let test = freeSpace.trim().toLowerCase();
 
-console.log(freeSpace.trim().toUpperCase().length);
-// console.log(test.length);
+// console.log(freeSpace.trim().toUpperCase().length);
+// console.log(test);
 
-let sport = 'BasketBall';
-
-let findSport = sport.toLowerCase().indexOf('bfall');
-
-if (findSport >= 0) {
-  let test = sport.slice(findSport);
-  console.log(test);
-
+if (test.indexOf('Colors')) {
+  let newWord = freeSpace.replace('Colors', 'Blue');
+  console.log(newWord);
 } else {
-  console.log('Not that word');
+  console.log('That is not that word');
 }
+
+
+
+
+// let sport = 'BasketBall';
+
+// let findSport = sport.toLowerCase().indexOf('bfall');
+
+// if (findSport >= 0) {
+//   let test = sport.slice(findSport);
+//   console.log(test);
+
+// } else {
+//   let newSport = sport.replace('BasketBall', 'football');
+//   console.log('New sport is ' + newSport);
+// }
+
+
