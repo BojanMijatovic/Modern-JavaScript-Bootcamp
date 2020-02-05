3cb9f8e16f8c207d9d9dbf35a8126e863753bdb4
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-02-05 20:50:40 +0100

    more on strings

--- script/basic/strings.js
+++ script/basic/strings.js
@@ -3,9 +3,9 @@
 
 let name = 'Bojan';
 
-let lastName = 'Mijatovic';
+// let lastName = 'Mijatovic';
 
-let fullName = name + ' ' + lastName;
+// let fullName = name + ' ' + lastName;
 
 let city = 'Novi Sad';
 let country = 'Serbia';
@@ -13,9 +13,26 @@ let location = ' i come form ' + city + ' in ' + country;
 
 // console.log(fullName);
 
-console.log('Hello my name is ' + fullName + location);
+// console.log('Hello my name is ' + fullName + location);
 
 //  New Lines off text
 let newLineText = 'We have first line text \n and Second line here';
 
-console.log(newLineText);
+// console.log(newLineText);
+
+let firstName = 'Ziggy';
+let lastName = 'Canary';
+
+let fullName = 'You are ' + firstName + ' ' + lastName;
+// console.log(fullName);
+
+let num = fullName.length;
+// console.log(num); length off full name with spaces
+
+let firstChar = firstName[0];
+
+// console.log(firstChar);  'Z'
+
+let msg = 'Here is first chat';
+
+// console.log(msg.toUpperCase()); HERE IS FIRST CHAT
