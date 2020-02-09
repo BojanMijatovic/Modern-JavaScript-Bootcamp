ae733b88f94662f42bcbffd87701ffbeb08ccc21
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-02-09 13:40:55 +0100

    array

--- script/basic/logical-and-or.js
+++ script/basic/logical-and-or.js
@@ -58,9 +58,27 @@ let amountStatus = 10;
 
 
 
-let num = Math.round(Math.random() * 9 + 1);
-if (num >= 4 && num <= 8) {
-  console.log('Great num');
-} else {
-  console.log(`Num ${num} is small or high`);
-}
+// let num = Math.round(Math.random() * 9 + 1);
+// if (num >= 4 && num <= 8) {
+//   console.log('Great num');
+// } else {
+//   console.log(`Num ${num} is small or high`);
+// }
+
+
+// let age = 31;
+
+// if (age < 18 || age > 60) {
+//   console.log('You are not allowed to enter in disco');
+// } else {
+//   console.log(`Welcome you have ${age} years`);
+// }
+
+//let a = 7;
+
+// console.log(a === 7 || a > 10 && a == 3);  // true
+
+// (a === 7 || a > 10 )&& a == 3 // false
+
+
+
---
+++ script/basic/switch.js
@@ -0,0 +1,47 @@
+
+
+// let book = 2;
+
+// switch (book) {
+//   case 1: console.log('Wolf');
+//     break;
+//   case 2: console.log('LOTR');
+//     break;
+//   case 3: console.log('This is a book');
+//     break;
+//   case 4: console.log('Romance');
+//     break;
+//   default: console.log(`We don t have a ${book}`);
+//     break;
+// }
+
+// let product = 'paper';
+
+
+// switch (product) {
+//   case 'metal': console.log('This is metal');
+//     break;
+//   case 'paper': console.log('This is paper');
+//     break;
+//   case 'box': console.log('This is box');
+//     break;
+//   case 'rock': console.log('This is rock');
+//     break;
+//   default: console.log('We don t have that material');
+//     break;
+// }
+
+// let num = 5;
+
+// num == 7 ? console.log('True') : console.log('False');
+
+// let userStatus = 'offline';
+
+// userStatus === 'online' ? console.log('Welcome') : console.log('Please sing up');
+
+// let color = 'red';
+
+// userStatus === 'offline' ? color : color = 'green';
+// console.log(color);
+
+
