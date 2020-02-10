a97bb0206784a1ced908132a95bd45a1cfd3c644
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-02-10 21:05:23 +0100

    practice

--- script/basic/objects.js
+++ script/basic/objects.js
@@ -9,7 +9,7 @@ const fitBitData = {
 }
 
 let userName = fitBitData.userName;
-console.log(userName);
+// console.log(userName);
 
 const users = {
   userName: null,
@@ -19,11 +19,81 @@ const users = {
 users.userName = 'Freddy';
 users.star = 5;
 
-console.log(users);
-
 users.addNewUser = 'Tom';
 users.addNewRatting = 3.4;
 
 //  Update ratting 
 users.addNewRatting += 2;
-console.log(users);
+// console.log(users);
+
+const student = {
+  firstName: 'Ted',
+  lastName: 'Morison',
+  exams: {
+    midTerm: 92,
+    final: 84
+  },
+  strengths: ['Music', 'Art']
+}
+
+let finalScore = (student.exams.midTerm + student.exams.final) / 2;
+// console.log(`Final score of ${student.firstName} ${student.lastName} is ${finalScore} points`);
+
+let studentArt = student.strengths.join('/');
+// console.log(`Student strengths are ${studentArt}`);
+
+const shopCart = [
+  {
+    nameProduct: 'player',
+    price: 300,
+    quantity: 1
+  },
+  {
+    nameProduct: 'T-Shirt',
+    price: 35,
+    quantity: 3
+  },
+  {
+    nameProduct: 'shoes',
+    price: 100,
+    quantity: 1
+  }
+]
+
+const game = {
+  player1: {
+    playerName: 'John',
+    playerLastName: 'Doe',
+    end: false,
+    score: null
+  },
+  player2: {
+    playerName: 'Rex',
+    playerLastName: 'Terrie',
+    end: false,
+    score: null
+  }
+}
+
+//  Car Obj
+const car = {
+  name: 'Ford',
+  type: '350x',
+  gearNum: 5,
+  maxSpeed: 350
+}
+
+let addNumDoors = car.numDoors = 4;
+//  add Max Speed
+if (car.gearNum >= 5) {
+  car.maxSpeed += 10;
+}
+
+//  Rename Type and Name
+if (car.name.includes('F')) {
+  car.name = 'New Line Ford';
+  car.type = '700z'
+} else {
+  car.name = 'Ford';
+}
+console.log(car);
