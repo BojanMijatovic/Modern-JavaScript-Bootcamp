dd1888c51a2f2694db857bed2ea89358c8deb665
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-02-10 19:38:50 +0100

    simple crud

--- script/basic/library.js
+++ script/basic/library.js
@@ -6,11 +6,25 @@ library.push('react');
 library.push('js');
 library.push('redux');
 library.push('advanced Js');
+
 console.log(library);
 
-let searchBook = library.indexOf('react');
+//  Search Index off book 
+
+let searchBook = library.indexOf('redux');
+console.log(searchBook);
+
+//  Remove Book 
+if (searchBook >= 0) {
+  library.splice(searchBook, 1);
+}
+
+// Edit Title book
 
 if (searchBook >= 0) {
-  console.log(library[searchBook]);
+  library.splice(searchBook, 1, 'GraphQL');
 }
+
+console.log(library);
+
 
