a510c76de2a82d67c95e19d15dfe30716b1cfa19
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-02-11 20:48:44 +0100

    start with loops

---
+++ script/basic/loops.js
@@ -0,0 +1,34 @@
+
+//  Standard For loop
+// for (let i = 1; i <= 10; i++) {
+//   console.log('Hello ' + [i]);
+// }
+
+// Multiply
+// for (let i = 1; i < 10; i++) {
+//   console.log(`${i * i}`);
+// }
+
+
+// Back
+for (let i = 50; 0 <= i; i -= 10) {
+  // console.log(i);
+}
+
+//  More back loop  :)
+for (let i = 200; i >= 0; i -= 20) {
+  // console.log(i);
+}
+
+// Task num 3
+for (let i = 0; i <= 10; i++) {
+  if (i % 2 === 0) {
+    // console.log(i);
+  }
+}
+// Task num 4
+for (let i = 0; i <= 20; i++) {
+  if (i % 3 === 0) {
+    // console.log(i);
+  }
+}
