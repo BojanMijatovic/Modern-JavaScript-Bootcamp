24a33b109795e8b3389335c44f3f89a02436e8d8
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-02-16 19:12:23 +0100

    add wheel

--- script/objects/car.js
+++ script/objects/car.js
@@ -1,4 +1,27 @@
 
+//  Random nums 
+
+// let randomNum = Math.floor(Math.random() * 6) + 1;
+// let makeGes = 4;
+
+// let random = function (gesNum, ranNum) {
+//   if (gesNum === ranNum) {
+//     return true
+//   } else {
+//     return false;
+//   }
+// }
+
+// let a = random(makeGes, randomNum);
+// console.log(a);
+// console.log(randomNum);
+
+//  Todo array
+
+// const todoArr = ['js', 'node', 'react', 'redux', 'robotic'];
+// console.log(`Todo to ${todoArr[0]} and ${todoArr[todoArr.length - 2]}`);
+
+
 const myCar = {
   name: 'ford',
   speed: 0,
@@ -8,16 +31,16 @@ const myCar = {
 
 //  Start Car
 let startCar = function (car, sp, gear) {
-  let startG = car.gear += gear;
+  car.gear = car.gear += gear;
   let startingCar = car.speed += sp;
-  if (startG > 0 && startingCar <= 15) {
-    console.log(`You start ${car.name} with speed ${startingCar} in ${startG} gear`);
+  if (gear > 0 && startingCar <= 15) {
+    console.log(`You start ${car.name} with speed ${startingCar} in ${gear} gear`);
   }
   car.break = false;
 }
 
-let a = startCar(myCar, 10, 1);
-console.log(myCar);
+// let a = startCar(myCar, 10, 1);
+
 
 //  Stop car 
 let stopCar = function (car) {
@@ -27,5 +50,30 @@ let stopCar = function (car) {
   return `You are now stop ${car.name} speed is  ${car.speed} and gear are ${car.gear}`
 }
 
-let b = stopCar(myCar)
-console.log(b);
+// let b = stopCar(myCar)
+// console.log(b);
+
+// console.log(myCar);
+
+
+
+const wheelProps = {
+  num: 4,
+  height: 103,
+  width: 205,
+  revsPerMile: 874,
+  carSpeed: 0
+}
+
+
+let speedWheel = function (wheel, speed) {
+  wheel.carSpeed = wheel.carSpeed += speed;
+  if (wheel.carSpeed >= 10) {
+    wheel.revsPerMile += speed;
+    wheel.width = wheel.width += (speed / 2);
+    wheel.height = wheel.height -= (speed / 2);
+  }
+}
+// let a = speedWheel(wheelProps, 12);
+
+// console.log(wheelProps);
