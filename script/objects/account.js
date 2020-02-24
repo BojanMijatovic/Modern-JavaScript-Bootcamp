7c3fe70db038b7c09d5d2771eaf2983b0991f3b5
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-02-24 20:20:08 +0100

    add

--- script/objects/account.js
+++ script/objects/account.js
@@ -8,10 +8,7 @@ const addExpenses = (description, amount) => {
   account.expenses.push({ description, amount });
 }
 
-
 addExpenses('Rent', 700);
 addExpenses('Coffee', 10);
 console.log(account);
 
-
-console.log(test);
