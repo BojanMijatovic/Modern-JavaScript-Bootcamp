079103ed2c50a7c3d0d58fda548a1652ede747d5
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-02-27 21:42:45 +0100

    Destructing

--- script/objects/destrucingArr.js
+++ script/objects/destrucingArr.js
@@ -20,10 +20,38 @@ const nums = {
   y: 5
 }
 
+//  Destructing obj
 const multiply = (ter) => {
   const { x, y, z } = ter;
   return z * y * x;
 }
 
-console.log(multiply(nums));
+// console.log(multiply(nums));
 
+
+const runner = {
+  name: 'John',
+  last: 'Doe',
+  age: 31
+}
+
+
+// const fullName = (person) => {
+//   const { name, last } = person;
+//   console.log(`${name} and ${last}`);
+// }
+
+// fullName(runner);
+
+const printName = ({ name, last }) => console.log(`Name is ${name} and last name is ${last}`);
+// printName(runner)
+
+//  Destructing  arr 
+const response = [
+  'http/ 21.444.',
+  'status 200',
+  'json'
+]
+
+const checkStatus = ([address, status, file]) => console.log(`on address ${address} you have status ${status} with file ${file}`);
+checkStatus(response)
