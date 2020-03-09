1a3564270629ccbeaa9acbbb9687ab1725384e06
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-03-09 20:15:41 +0100

    dom manipulation

--- script/dom/firstDom.js
+++ script/dom/firstDom.js
@@ -1,1 +1,20 @@
-console.log('test');
+
+const btnId = document.getElementById('btnSubmit');
+
+//console.log(btnId);
+
+const lists = document.getElementsByTagName('p');
+// console.log(lists);
+
+for (let list of lists) {
+  //console.log(list);
+}
+// console.log(ul.getElementsByClassName('list'));
+
+
+const title = document.querySelector('h1');
+console.log(title);
+
+
+const paragraph = document.querySelectorAll('p');
+console.log(paragraph);
