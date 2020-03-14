5bd6d027f376691fd8cc930af7d9f95692246304
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-03-14 10:59:03 +0100

    papier , rock , scissors

--- script/functions/functions-01-starting-code/app.js
+++ script/functions/functions-01-starting-code/app.js
@@ -16,19 +16,53 @@ const startGameBtn = document.getElementById('start-game-btn');
 const rock = 'rock';
 const papier = 'papier';
 const scissors = 'scissors';
+const defUserChoice = 'rock'
 
+const draw = 'draw';
+const playerWin = 'Player is Winner';
+const compWin = 'Computer is Winner';
+
+let gameIsRun = false;
 
 const getStartGame = function () {
-  const selection = prompt('Papier , Rock , Scissors', '');
+  const selection = prompt(`${rock} ${papier} ${scissors}`, '').toLowerCase();
   if (selection !== rock && selection !== papier && selection !== scissors) {
-    return alert(`Sorry you need to use some off Papier , Rock , Scissors`)
+    alert(`Sorry you need to use some off papier , rock , scissors , we choose for you  ${defUserChoice}`)
+    return defUserChoice;
   }
   return selection;
 }
 
+const getCompChoice = function () {
+  const radomValue = Math.random();
+  if (radomValue < 0.34) {
+    return rock;
+  } else if (radomValue < 0.67) {
+    return papier;
+  } else {
+    return scissors;
+  }
+}
+
+
+const getWinner = function (cChoice, uChoice) {
+  if (cChoice === uChoice) {
+    return draw;
+  } else if (cChoice === rock && uChoice === papier || cChoice === papier && uChoice === scissors || cChoice === scissors && uChoice === rock) {
+    return playerWin
+  } else {
+    return compWin
+  }
+}
 
 
 startGameBtn.addEventListener('click', function () {
+  if (gameIsRun) {
+    return;
+  }
+  gameIsRun = true;
   const playerSelection = getStartGame();
-  console.log(playerSelection);
+  const getComputerChoice = getCompChoice();
+  const winner = getWinner(getComputerChoice, playerSelection);
+  console.log(winner);
 });
