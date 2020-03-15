9b078228683abcf4609d1d0bcc27080794f5343d
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-03-15 11:39:59 +0100

    Change property with class

--- script/dom/dom-01-starting-code/app.js
+++ script/dom/dom-01-starting-code/app.js
@@ -6,3 +6,19 @@ p.innerText = 'New to Dom';
 p.className = 'test';
 
 document.body.appendChild(p);
+
+// Get element by ID selector 
+// const title = document.getElementById('main-title');
+// console.log(title);
+
+// console.dir(title)
+
+//querySelector ALL!! ali items with that class
+const list = document.querySelectorAll('.list-items');
+console.log(list);
+
+//  Change property with class
+
+const h1 = document.querySelector('#main-title');
+h1.innerText = 'New Title on DOM';
+h1.className = 'title';
