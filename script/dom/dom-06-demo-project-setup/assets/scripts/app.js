de8cda89cd3c83446806295245bf3350b440b560
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-03-17 21:31:10 +0100

    Controlling the Backdrop

--- script/dom/dom-06-demo-project-setup/assets/scripts/app.js
+++ script/dom/dom-06-demo-project-setup/assets/scripts/app.js
@@ -1,11 +1,32 @@
 
 const startAddMovie = document.getElementById('add-movie');
-
 const modal = document.getElementById('add-modal');
+const cancelMovieBtn = document.querySelector('#add-modal .btn--passive');
 
 
-startAddMovie.addEventListener('click', () => {
-  console.log(modal);
-  modal.classList = '.modal.visible';
+//  Show backDrop 
+const backdrop = document.getElementById('backdrop');
+const showBackdropToggle = () => {
+  backdrop.classList.toggle('visible');
 }
-)
+
+
+//  Show or hide movie modal
+const toggleMovieModal = () => {
+  modal.classList.toggle('visible');
+  showBackdropToggle();
+}
+
+// cancel backDrop 
+const backdropClickHandler = () => {
+  toggleMovieModal();
+}
+
+//  Cancel addMovie 
+const cancelAddMovie = () => {
+  toggleMovieModal();
+}
+
+startAddMovie.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelMovieBtn.addEventListener('click', cancelAddMovie);
