864a061ff1283b94e368b21930f35391a8e44fe1
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-03-18 21:33:21 +0100

    easy for now

--- script/dom/domPlayPlace/script.js
+++ script/dom/domPlayPlace/script.js
@@ -1,1 +1,46 @@
-console.log('test');
+
+const title = document.getElementById('main-title');
+const changeTitle = document.getElementById('change-title');
+const addName = document.getElementById('add-name');
+const nameValue = document.getElementById('input-name');
+const changeColor = document.querySelector('.changeColor');
+const list = document.querySelector('.first-list');
+names = [];
+
+
+
+//  Toggle background color 
+const changeBackground = () => {
+  const body = document.querySelector('body');
+  body.classList.toggle('main-color');
+}
+
+//  Clear input 
+const clearInput = () => {
+  nameValue.value = '';
+}
+
+
+// Add names 
+const addNameHandler = () => {
+
+  const newName = document.createElement('li');
+  newName.innerText = nameValue.value;
+  newName.classList = 'listStyle';
+  names.push(newName);
+  list.append(newName);
+  clearInput();
+}
+
+
+//  Change Title Value
+
+const changeTitleValue = () => {
+  title.innerText = 'This is good beginning';
+  title.classList = 'main-title';
+}
+
+
+changeTitle.addEventListener('click', changeTitleValue)
+addName.addEventListener('click', addNameHandler)
+changeColor.addEventListener('click', changeBackground);
