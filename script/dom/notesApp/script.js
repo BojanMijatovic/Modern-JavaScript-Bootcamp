87b85e550e0dd7e2b463f79fc5b9fa84b6569702
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-03-19 21:14:26 +0100

    Advanced Queries

--- script/dom/notesApp/script.js
+++ script/dom/notesApp/script.js
@@ -8,25 +8,29 @@ const notes = [{
 }, {
   title: 'Dom practice',
   body: 'work on simple projects'
-}
-]
-
-
+}]
 
 const p = document.querySelector('p');
-
 // p.remove();
-
 
 //  Add all p tags in one element
 const allP = document.querySelectorAll('p');
 //  Remove all p tags
 // allP.forEach(p => p.remove());
 
-allP.forEach(p => p.textContent = `*****`);
+// allP.forEach(p => p.textContent = `*****`);
+
+// const newP = document.createElement('p');
+// newP.textContent = `This is new paragraph`;
+
+// document.body.append(newP);
+
+const addBtn = document.getElementById('create-note');
+
+const addNote = (e) => {
+  e.target.textContent = 'This is new btn';
+}
 
 
-const newP = document.createElement('p');
-newP.textContent = `This is new paragraph`;
 
-document.body.append(newP);
+addBtn.addEventListener('click', addNote);
--- script/dom/todoApp/script.js
+++ script/dom/todoApp/script.js
@@ -14,10 +14,37 @@ const todos = [
   }
 ]
 
-const allP = document.querySelectorAll('p');
+// const allP = document.querySelectorAll('p');
 
-allP.forEach(p => {
-  if (p.textContent.includes('Work')) {
-    p.remove();
-  }
+// allP.forEach(p => {
+//   if (p.textContent.includes('Work')) {
+//     p.remove();
+//   }
+// })
+
+//  count todos 
+const todoCount = () => {
+
+  todos.forEach(todo => {
+    if (todo.completed === false) {
+      const newP = document.createElement('p');
+      newP.textContent = `You have ${todo.text} to complete`;
+      document.body.append(newP);
+    }
+  })
+}
+
+todoCount(todos);
+
+//  filter todos
+const filterIncompleteTodos = todos.filter(todo => !todo.completed);
+
+const filterTodos = document.createElement('h2');
+filterTodos.textContent = `You have more ${filterIncompleteTodos.length} to complete`;
+document.body.append(filterTodos);
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', () => {
+  console.log(`You just click on btn`);
 })
