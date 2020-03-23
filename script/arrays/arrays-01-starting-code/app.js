c10f884fff7f15f009607160e38aa84bc86cbd65
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-03-23 12:56:34 +0100

    Transforming Data with map

--- script/arrays/arrays-01-starting-code/app.js
+++ script/arrays/arrays-01-starting-code/app.js
@@ -3,7 +3,7 @@ const numbers = [1, 2, 3, 4];
 //console.log(numbers);
 
 const newArr = new Array(); // obj constructor
-console.log(newArr);
+// console.log(newArr);
 const li = document.querySelectorAll('li');
 // console.log(li);
 
@@ -15,7 +15,60 @@ const test = Array.from(li)
 const hobbies = ['Js', 'React'];
 hobbies.push('Node.js'); // add on end array
 hobbies.unshift('Html , Css'); // add on begin array
-console.log(hobbies);
+// console.log(hobbies);
 
 const removeLast = hobbies.pop();
-console.log(`Last removed is ${removeLast} in arr ${hobbies}`);
+// console.log(`Last removed is ${removeLast} in arr ${hobbies}`);
+
+hobbies.splice(hobbies.length, 0, 'Ex Airsoft', 'Node.js');
+// console.log(hobbies);
+
+const removeAll = () => {
+  hobbies.splice(0, hobbies.length) // remove all from array
+}
+// removeAll();
+// console.log(hobbies);
+
+const testResults = [1, 2, 3.4, -1, 10];
+const storedResult = testResults.slice(0, 1);
+// console.log(storedResult);
+
+const resultIndex = testResults.indexOf(10);
+// console.log(resultIndex);
+
+
+const person = [{ name: 'John', age: 21 }, { jobs: 'freelance', type: 'backend' }];
+
+
+const findJobs = person.find((p, id, persons) => { // return match obj
+  return p.name === 'John';
+});
+// console.log(findJobs);
+
+
+const findIndexJob = person.findIndex((person, id) => { // return index off
+  return person.name === 'John';
+})
+// console.log(findIndexJob);
+
+
+const prices = [2, 44, 5.2, 1.3];
+const taxes = 0.2;
+const newPrice = []; // ----  empty arr to add new Prices 
+// const addTax = prices.map((price) => {  // map function  
+//   return price * taxes;
+// });
+// console.log(`This is price with tax ${addTax}`);
+
+const addTax = prices.forEach((price) => {
+  return newPrice.push(price + (price * taxes)); // For each function
+})
+// console.log(prices);
+// console.log(newPrice);
+
+const shopPrice = prices.map((price, id) => {
+  const item = { index: id + 1, amount: price }; // example for shop and prices
+  return item;
+});
+
+console.log(shopPrice);
