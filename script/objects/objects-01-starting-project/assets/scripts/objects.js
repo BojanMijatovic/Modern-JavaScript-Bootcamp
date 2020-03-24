ef6dca06fed8f071ff865ee63b118839f49a96b2
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-03-24 13:48:43 +0100

    Outputting Dynamic Properties

--- script/objects/objects-01-starting-project/assets/scripts/objects.js
+++ script/objects/objects-01-starting-project/assets/scripts/objects.js
@@ -1,20 +1,68 @@
+const addMovieBtn = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
+const movies = [];
 
-const person = {
-  name: 'John',
-  age: 32,
-  hobbies: ['Sports', 'Airsoft'],
-  greet: function () {
-    alert('Hi there');
+
+//  Render Movie
+const renderMovies = (filter = '') => {
+  const movieList = document.getElementById('movie-list');
+
+  if (movies.length === 0) {
+    movieList.classList.remove('visible');
+  } else {
+    movieList.classList.add('visible');
   }
+  movieList.innerHTML = '';
+
+  const filteredMovies = !filter ? movies : movies.filter(movie => movie.info.title.includes(filter))
+
+
+  filteredMovies.forEach((movie) => {
+    const moveEl = document.createElement('li');
+    let text = movie.info.title + ' - ';
+
+    for (const key in movie.info) {   //  Dynamic Properties
+      if (key !== 'title') {
+        text = text + `${key}: ${movie.info[key]}`
+      }
+    }
+    moveEl.textContent = text; //Outputting Dynamic Properties
+    movieList.append(moveEl);
+  }
+  )
 }
 
-// console.log(person);
+//  Add Movie
+const addMovieHandler = () => {
+  const title = document.getElementById('title').value;
+  const extraName = document.getElementById('extra-name').value; // Dynamic Properties
+  const extraValue = document.getElementById('extra-value').value;
 
-person.age = 13;
-// console.log(person);
+  //  Validation
+  if (title.trim() === '' || extraName.trim() === '' || extraValue.trim() === '') {
+    alert('Please insert values')
+  }
 
-const remPerson = () => {
-  delete person.age;
+  //   New Movie obj
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue //  Dynamic stored Name
+    },
+    id: Math.random()
+  }
+  movies.push(newMovie);
+  renderMovies();
 }
-// remPerson();
-// console.log(person);
+
+// Search Movie
+
+const searchMovieHandler = () => {
+  const filterTerm = document.getElementById('filter-title').value;
+  renderMovies(filterTerm)
+}
+
+
+
+addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler);
