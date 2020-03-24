1a0042153c0d3f38219390b7bf7cfbe6efbf23ec
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-03-24 20:30:30 +0100

    some tests

--- script/dom/dom-01-starting-code/app.js
+++ script/dom/dom-01-starting-code/app.js
@@ -1,15 +1,10 @@
 const p = document.createElement('p');
-
 p.innerText = 'New to Dom';
 p.className = 'test';
 document.body.appendChild(p);
 
 // Get element by ID selector 
 // const title = document.getElementById('main-title');
-// console.log(title);
-
-// console.dir(title)
-
 //querySelector ALL!! ali items with that class
 const list = document.querySelectorAll('.list-items');
 //  Change property with class
@@ -19,18 +14,13 @@ h1.className = 'title';
 
 
 //console.log(input.value);
-
-
 const ul = document.querySelector('ul');
-
 // console.log(ul.firstElementChild.hidden = true);
-
 const li = document.querySelector('li');
 
 // console.log(li.parentElement.parentElement);
 
 // Style With DOM
-
 const btn = document.querySelector('button');
 
 btn.addEventListener('click', () => {
@@ -54,28 +44,26 @@ ul.removeChild(hed);
 // Remove Element
 // h1.remove();
 
-const items = [];
 
+const items = [];
+const input = document.querySelector('#inputValue').value;
+const newUl = document.querySelector('.newItems');
 const addBtn = document.getElementById('add-item');
+const searchBtn = document.getElementById('src-btn');
 
 
 const addItem = () => {
-  //  Input text
-  const input = document.querySelector('#inputValue').value;
-  const newUl = document.querySelector('.newItems');
 
   //  Validation
   if (input.trim() === '') {
     alert('Please insert some item')
   }
-
   const newLi = document.createElement('p');
   newLi.textContent = input;
   newUl.append(newLi);
-
-  items.push(input);
+  items.push(input)
+  console.log(items);
 
 }
-
 
 addBtn.addEventListener('click', addItem);
