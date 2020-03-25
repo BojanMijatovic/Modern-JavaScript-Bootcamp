672d8941b3ed45192b2f374536c77b8f275c8a8d
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-03-25 14:00:12 +0100

    start with sample

---
+++ oop/oop Covid-19/script.js
@@ -0,0 +1,67 @@
+class Doctor {
+  constructor(name, experience, profession) {
+    this.name = name;
+    this.experience = experience;
+    this.profession = profession;
+  }
+}
+
+class Patient {
+  constructor(name, status, illness) {
+    this.name = name;
+    this.status = status;
+    this.illness = illness;
+  }
+}
+
+class Hospital {
+  doctors = [
+    new Doctor('Mike', 4, 'Epidemiology'),
+    new Doctor('Alex', 10, 'Lung Deceases')
+  ]
+
+  addDoctors() {
+    this.doctors.push(new Doctor('John', 5, 'Practitioner'))
+  }
+
+  showDoctorStatus() {
+    for (const doctor of this.doctors) {
+      const showDoctors = document.createElement('li');
+      showDoctors.innerHTML = `<p> Doctor ${doctor.name} Experience ${doctor.experience} years profession ${doctor.profession}</p>`;
+      document.getElementById('app').append(showDoctors);
+    }
+  }
+
+  patients = [
+    new Patient('Scot', 'ill', 'covid-19'),
+    new Patient('Test', 'car accident', 'critical')
+  ]
+
+  addPatients() { }
+
+  showPatientsStatus() {
+    for (const patient of this.patients) {
+      const newPatient = document.createElement('li');
+      newPatient.innerHTML = `<p>Patient ${patient.name} status ${patient.status} ${patient.illness}</p>`;
+      document.getElementById('patients').append(newPatient);
+    }
+  }
+}
+
+
+const addDoctors = new Hospital()
+const addPatients = new Hospital();
+
+addDoctors.addDoctors();
+
+console.log(addDoctors);
+
+addPatients.addPatients();
+console.log(addPatients);
+
+
+const showHospitalStatus = new Hospital();
+showHospitalStatus.showDoctorStatus();
+
+const showPatients = new Hospital();
+showPatients.showPatientsStatus();
