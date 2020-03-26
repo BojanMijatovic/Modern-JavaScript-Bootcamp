96a047883e8a681f087b98e80e1dc79cee5ab960
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-03-26 12:38:47 +0100

    change Id

--- oop/oop Account/script.js
+++ oop/oop Account/script.js
@@ -17,17 +17,16 @@ class Bank {
     new Account('Tom', 'Andrew', 400, 35, 44171)
   ]
 
-
-  showAccount() {
+  showAccount() {               //    show status on html
     for (const acc of this.accounts) {
       const accItem = document.createElement('li');
-      accItem.innerHTML = `<p>${acc.name} ${acc.lastName} have ${acc.balance}\$ and income is ${acc.income}</p>`;
+      accItem.innerHTML = `<p>${acc.name} ${acc.lastName} have ${acc.balance}\$ and income is ${acc.income} with ${acc.id}</p>`;
       document.getElementById('app').append(accItem);
     }
   }
 
   addIncome() {
-    this.accounts.forEach((account) => {
+    this.accounts.forEach((account) => {           // add income for all 
       return console.log(`${account.name} have income off ${account.income + 100}\$`);
     }
     )
@@ -39,11 +38,27 @@ class Bank {
     }
     )
   }
+
+  addAccount() {
+    this.accounts.push(new Account('Henry', 'Red', 887, 278, 45646));
+    this.accounts.forEach(ac => console.log(`Now we have ${ac.name} i our bank`))
+    this.changeId();
+    this.showAccount();
+  }
+
+  removeAccount() {
+    this.accounts.splice(1, 1);
+    // console.log(this.accounts);
+    this.addAccount();
+  }
+
+  changeId() {
+    const newId = 1344000;
+    this.accounts.forEach(acc => acc.id += newId);
+  }
 }
 
-const addAccount = new Bank(new Account);
 const add2 = new Bank(new Account);
-add2.showAccount()
 
 const addIncome = new Bank(new Account);
 addIncome.addIncome();
@@ -51,5 +66,8 @@ addIncome.addIncome();
 const payTax = new Bank(new Account);
 payTax.removeFromBalance();
 
-console.log(add2);
+const removeAc = new Bank(new Account);
+removeAc.removeAccount()
 
+const newId = new Bank(new Account);
+newId.changeId();
