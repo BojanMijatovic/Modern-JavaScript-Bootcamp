3d5e404de13c6f388fd6865813e23ebd6f207293
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-03-30 11:48:13 +0200

    prototype

---
+++ oop/prototypes-01-starting-setup/app.js
@@ -0,0 +1,61 @@
+
+
+// function Person() {
+//   this.name = 'Alex';
+//   this.age = 30;
+//  this.greet = function () {
+//      return console.log(`Hi my name is ${this.name} i have ${this.age} years`);
+// }
+//   calc = () => {
+//     return `Hello ${this.name}`
+//   }
+
+// }
+
+// const p = new Person();
+
+
+// function newName() {
+//   this.name = 'Max';
+//   hi = function () {
+//     console.log(`Hi my name is ${name}`);
+//   }
+// }
+
+
+// class Test {
+//   hello = () => {
+//     console.log('Hello again');
+//   }
+// }
+
+
+// const e = new Test();
+// const c = document.querySelector('button');
+// c.addEventListener('click', e.hello)
+
+// console.log(newName.hi());
+
+
+
+
+
+// function Person() {
+//   this.name = 'Max';
+//   this.greet = function () {
+//     console.log(`Hello ${this.name}`);
+//   }
+// }
+
+// const t = new Person();
+// t.greet();
+
+
+
+// function Person() {
+//   this.name = 'Max';
+//   this.greet = () => console.log(`Welcome ${this.name}`);
+// }
+
+// const t = new Person();
+// t.greet();
