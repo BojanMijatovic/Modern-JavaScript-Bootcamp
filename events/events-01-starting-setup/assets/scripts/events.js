878d5790acc3c67e40f9c512164add3267edfd6b
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-04-01 13:56:19 +0200

    more on functions

---
+++ events/events-01-starting-setup/assets/scripts/events.js
@@ -0,0 +1,27 @@
+
+// const btn = document.querySelector('button');
+
+// const btnClickHandler = (event) => console.log(event.target.textContent);
+
+
+
+// btn.addEventListener('click', btnClickHandler);
+
+// setTimeout(() => {
+//   btn.removeEventListener('click', btnClickHandler);
+// }, 2000);
+
+
+// const form = document.querySelector('form');
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   console.log(e);
+// }
+// )
+
+
+  // --- Pure Functions 
+
+
+// const add = (x, y) => x + y;
+// console.log(add(2, 3));
