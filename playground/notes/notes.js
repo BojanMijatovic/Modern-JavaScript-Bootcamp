a105865e41c5c9b57be51b3ae20da8e3e4a227d2
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-04-15 13:11:30 +0200

    methods

--- playground/notes/notes.js
+++ playground/notes/notes.js
@@ -3,18 +3,66 @@ const notes = [
   { title: 'Learn JS', body: 'Work every day on JS' },
   { title: 'Go to gym', body: 'Practice when you have spare time' },
   { title: 'Walk to center', body: 'Take a walk' },
-  { title: 'Learn fucking JS !!', body: ' Work clever and always' }
+  { title: 'Learn fucking JS !!', body: ' Work clever and always' },
+  { title: 'Attack on Dom JS  :)', body: ' Work clever and always' }
 ];
 
+// console.log(notes);
+// notes.shift();
+// let note = notes.indexOf('Feed cat');  // find index in array of notes
+
+// findIndex work on obj.
+
+// return index off obj from arr notes
+const index = notes.findIndex((note, i) => note.title = 'Feed cat');  // find by title 
+console.log(index);
+
+const findNote = (notes, noteTitle) => {  // findNote is index of obj in array notes
+  return notes.findIndex((note) => note.title === noteTitle);
+}
+// console.log(findNote(notes, 'Learn JS'));
+
+// find Index method
+// const noteObj = (notes, noteTitle) => {
+//   const index = notes.findIndex(note => note.title.toLowerCase() === noteTitle.toLowerCase())
+//   return notes[index];                             // return obj note  from array with index
+// }
+
+
+// Find array method 
+const noteObj = (notes, noteTitle) => {
+  return notes.find(note => note.title.toLowerCase() === noteTitle.toLowerCase())
+  // return obj note  from array  notes  
+}
+
+// filter notes
+const filteredNotes = notes.filter(note => note.title.toLowerCase().includes('js'));
+// create new arr  from notes by filter title
+console.log(filteredNotes);
+
+
+// sort notes
+const sortNotes = (notes) => {
+  notes.sort((a, b) => {
+    if (a.title.toLowerCase() < b.title.toLowerCase()) {
+      return -1
+    } else if (b.title.toLowerCase() < a.title.toLowerCase()) {
+      return 1
+    } else {
+      return 0
+    }
+  })
+}
+
+sortNotes(notes);
+
+
+
+// const note = noteObj(notes, 'go to gym');
+// console.log(note);
 
 console.log(notes);
 
-// notes.shift();
-
-let note = notes.indexOf('Feed cat');  // find index in array of notes
 
 
 
-// findIndex work on obj.
-const index = notes.findIndex((note, i) => note.title = 'Feed cat');  // find by title 
-console.log(index);
--- playground/todo/todo.js
+++ playground/todo/todo.js
@@ -1,16 +1,50 @@
 
-const todos = ['Learn React', 'Angular', 'JavaScript', 'Redux', 'JSON', 'Flexbox'];
+const todos = [
+  { text: 'Learn React', completed: false },
+  { text: 'Angular', completed: false },
+  { text: 'Html 5', completed: true },
+  { text: 'JavaScript', completed: false },
+  { text: 'Redux', completed: false },
+  { text: 'Css 3', completed: true },
+  { text: 'JSON', completed: false },
+  { text: 'React Native', completed: false }
+];
 
-
-todos.splice(1, 1);
-todos.push('Work on UpWork')
-todos.shift();
-
-todos.forEach((todo, i) => console.log(`${i + 1} ${todo}`));      // for Each loop
-
-console.log(`You have ${todos.length} todos left`);
-
-
-for (let i = 0; i < todos.length; i++) {                          // with for loop
-  // console.log(`${i + 1} ${todos[i]}`);
+const deleteTodo = (todos, todoTitle) => {  // remove obj form array todos by text value
+  const todoIndex = todos.findIndex(todo => todo.text.toLowerCase() === todoTitle.toLowerCase());
+  if (todoIndex >= 0) {
+    todos.splice(todoIndex, 1)
+  }
 }
+
+deleteTodo(todos, 'angular');
+deleteTodo(todos, 'redux');
+
+
+// filtering todos
+const getThingsToDo = () => { // return new array with todos are false
+  return todos.filter(todo => !todo.completed)
+}
+
+
+// sort todos 
+const sortingTodos = (todos) => {
+  todos.sort((a, b) => {
+    if (a.completed < b.completed) {
+      return -1
+    } else if (b.completed < a.completed) {
+      return 1
+    } else {
+      return 0
+    }
+  })
+}
+
+
+sortingTodos(todos)
+console.log(todos);
+
+
+console.log(getThingsToDo(todos));
+
+
