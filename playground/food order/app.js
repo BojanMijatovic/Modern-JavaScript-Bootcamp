b0bdd00f2c3a9610cfcfd23dd41140bbf08d371e
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-04-29 21:43:04 +0200

    start food app

--- playground/food order/app.js
+++ playground/food order/app.js
@@ -1,13 +1,38 @@
-const orders = [];
+const orders = [{
+  food: 'burger',
+  spices: 'chili',
+  amount: 1,
+  completed: false
+},
+{
+  food: 'meat',
+  spices: 'origan',
+  amount: 1,
+  completed: false
+}, {
+  food: 'soup',
+  spices: 'chili',
+  amount: 1,
+  completed: true
+},
+{
+  food: 'pasta',
+  spices: 'cheese',
+  amount: 1,
+  completed: false
+}];
 
-const foodForm = document.querySelector('#foodOrder');
 
-const orderFood = (e) => {
-  e.preventDefault()
-  let food = document.querySelector('#addFood').value;
-  let amount = document.querySelector('#addAmount').value;
-  orders.push({ food: food, amountFood: amount })
-}
+// filter orders left
+const ordersLeft = orders.filter(order => !order.completed);    // return num orders with false completed
+const showOrdersLeft = document.createElement('p');
+showOrdersLeft.textContent = `You have more ${ordersLeft.length} orders to complete`;
+document.body.append(showOrdersLeft)
 
-foodForm.addEventListener('submit', orderFood)
+
+orders.forEach(order => {
+  const newOrder = document.createElement('p');
+  newOrder.textContent = `${order.food} with ${order.spices}`;
+  document.querySelector('.orders').append(newOrder);
+})
 
