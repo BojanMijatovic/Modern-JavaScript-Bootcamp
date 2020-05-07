933e958fe1abd63ab1c04a1b96b9c076276a0456
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-05-07 20:40:00 +0200

    Opening a Modal by Changing CSS Classes

---
+++ playground/asynchronous callbacks/main.js
@@ -0,0 +1,32 @@
+
+// console.log('First');   // 1
+
+// setTimeout(() => {
+//   console.log(`3 place`);     // 5
+// }, 3000);
+
+// console.log(`Second place`);  // 2
+// console.log(`Here is new`);   // 3
+
+// setTimeout(() => {
+//   console.log(`test`);
+//   setTimeout(() => {
+//     console.log(`Form inner func`);
+//   }, 1000);    //4
+// }, 2000);
+
+
+// const printName = (name, lastName) => {
+//   setTimeout(() => {
+//     console.log(name, lastName);
+//   }, 1000);
+// }
+
+// printName('Alex', 'Bex')
+
+
+const newP = new Promise((resolve, reject) => {
+
+})
+
+console.log(newP);
--- playground/events/main.js
+++ playground/events/main.js
@@ -1,29 +1,9 @@
-const title = document.querySelector('h1');
-title.textContent = 'This is new Title from js';
+const addNoteModal = document.querySelector('#add-note');
+const note = document.querySelector('.note');
 
-
-const subTitle = document.createElement('h2');
-subTitle.textContent = 'This is subtitle';
-document.body.append(subTitle);
-
-const colorBtn = document.querySelector('#change-color');
-
-colorBtn.addEventListener('click', (e) => {
-  title.classList.toggle('test');
-  title.textContent = 'New text';
-})
-
-const li = document.querySelectorAll('li');
-
-li.forEach(list => list.textContent = 'New title')
-
-
-const colors = ['red', 'green', 'blue'];
-
-for (let color of colors) {
-  const newC = document.createElement('p');
-  newC.textContent = 'test  new color'
-  newC.style.color = color;
-  document.body.append(newC)
+const addNoteHandler = () => {
+  note.classList.toggle('visible')
 }
 
+
+addNoteModal.addEventListener('click', addNoteHandler)
