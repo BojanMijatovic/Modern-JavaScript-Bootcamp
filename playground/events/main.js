6c26cc6ed4a91969e3a3bfcb28bd77b974ced3c7
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-05-07 21:31:13 +0200

    Backdrop

--- playground/events/main.js
+++ playground/events/main.js
@@ -1,9 +1,22 @@
 const addNoteModal = document.querySelector('#add-note');
 const note = document.querySelector('.note');
+const backdrop = document.querySelector('.backdrop');
+const cancelNoteBtn = document.querySelector('.notes__actions .cancel');
+
 
 const addNoteHandler = () => {
-  note.classList.toggle('visible')
+  note.classList.toggle('note-visible')   // show or hide note list
+  //  showBackdropToggle()
+}
+
+// const showBackdropToggle = () => {   // backdrop
+//   backdrop.classList.toggle('backdrop-visible');
+// }
+
+const showHideBackdrop = () => {
+  addNoteHandler();
 }
 
 
-addNoteModal.addEventListener('click', addNoteHandler)
+addNoteModal.addEventListener('click', addNoteHandler);
+backdrop.addEventListener('click', showHideBackdrop);
