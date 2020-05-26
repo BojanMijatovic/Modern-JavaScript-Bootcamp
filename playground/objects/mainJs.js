c15e44fe89383f276701d04ee7fedc3ddfd51740
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-05-26 21:31:08 +0200

    crud app

--- playground/objects/mainJs.js
+++ playground/objects/mainJs.js
@@ -1,26 +1,38 @@
 
+const products = [];
 
-const product = {
-  name: 'beer',
-  amount: 1,
-  price: 2.5,
-  sayHello: () => {
-    console.log(`Hold my ${product.name}`);
-  },
-  stateSale: {
-    state: 'Serbia',
-    city: 'Ns'
-  },
-  addProduct: (newP) => {
-    product.newProduct = { name: newP }
-  },
-  removeHello: () => {
-    delete product.sayHello
+const addProduct = (products, name, amount, value) => {
+  const newProduct = {
+    name,
+    amount,
+    value,
+    id: Math.random()
   }
+  products.push(newProduct)
 }
 
-// product.removeHello();
-// product.sayHello()
-// delete product.amount;
-product.addProduct('test');
-console.log(product);
+const showProducts = (products) => {
+  products.forEach((prod, id) => {
+    return console.log(`${id + 1}: ${prod.name} cost ${prod.value}$`);
+  })
+}
+
+const findProduct = (products, prodName) => {
+  const productIndex = products.filter(prod => prod.name.includes(prodName));
+  return console.log(productIndex);
+}
+
+
+
+addProduct(products, 'beer', 4, 0.2);
+addProduct(products, 'bred', 1, 0.15);
+addProduct(products, 'car', 1, 1200);
+addProduct(products, 'drugs', 20, 17);
+
+console.log(products);
+showProducts(products)
+
+findProduct(products, 'beer')
+
+findProduct(products, 'b')
+
