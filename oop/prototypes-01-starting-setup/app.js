dd4f82f57846e086a97ad488deefad9557f89b49
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-07-29 18:58:37 +0200

    Constructor Functions

--- oop/prototypes-01-starting-setup/app.js
+++ oop/prototypes-01-starting-setup/app.js
@@ -1,28 +1,53 @@
 
-const person = {
-  name: 'Alex',
-  lastName: 'Anderson',
-  married: true,
-  children: ['Ted', 'John'],
-  greet() {
-    console.log(`Hello my name is ${person.name}`);
-  },
-  job: {
-    title: 'developer',
-    company: false,
-    freelance: 'ast omega 021',
-    skills: {
-      frontEndDevelopment: ['html', 'css', 'js']
-    }
-  },
-  fullName() {
-    console.log(`My name is ${this.name}`);
-  }
+// const person = {
+//   name: 'Alex',
+//   lastName: 'Anderson',
+//   married: true,
+//   children: ['Ted', 'John'],
+//   greet() {
+//     console.log(`Hello my name is ${person.name}`);
+//   },
+//   job: {
+//     title: 'developer',
+//     company: false,
+//     freelance: 'ast omega 021',
+//     skills: {
+//       frontEndDevelopment: ['html', 'css', 'js']
+//     }
+//   },
+//   fullName() {
+//     console.log(`My name is ${this.name}`);
+//   }
+// }
+
+// person.town = 'Valencia';
+
+
+// person.fullName();
+// console.log(person);
+// console.log(person.job);
+
+
+// factory function
+// function createPerson(name, lastName, age) {
+//   return {
+//     name,
+//     lastName,
+//     age
+//   }
+// }
+
+// const ted = createPerson('ted', 'peters', 34);
+// console.log(ted);
+
+
+// constructor functions 
+function Car(model, power, type) {
+  this.model = model,
+    this.power = power,
+    this.type = type
 }
 
-person.town = 'Valencia';
 
-
-person.fullName();
-console.log(person);
-console.log(person.job);
+const bmw = new Car('m3', 'v16', 'mz');
+console.log(bmw);
